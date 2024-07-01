93268f6b952bcb270f797d99f8df0f074dc25e0f
redoy1102 <hridoyvlog9@gmail.com>
2024-07-01 16:41:39 +0800

    Project is ready to live

--- Milestone - 4 - Hello JS/Module-28/geometry-genius/scripts/area.js
+++ Milestone - 4 - Hello JS/Module-28/geometry-genius/scripts/area.js
@@ -7,6 +7,15 @@ function returnElement(id) {
     return document.getElementById(id);
 }
 
+function getTag(tagName) {
+    if (tagName == 'p') {
+        const p = document.createElement('p');
+        p.classList.add('my-5');
+        p.classList.add('mx-5');
+        return p;
+    }
+}
+
 function returnValue(id) {
     let element = returnElement(id);
     return element.value;
@@ -21,59 +30,90 @@ function areaValidation(area) {
     }
 }
 
+function capitalizeFirstCharacter(string) {
+    if (!string) {
+        return string
+    }
+    else {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+}
+
 function addToCalculationEntry(shapeName, area) {
+    shapeName = capitalizeFirstCharacter(shapeName);
     const calculationContainer = returnElement('calculation_container');
-    const p = document.createElement('p');
-    p.classList.add('my-5');
-    p.classList.add('mx-5');
+    const p = getTag('p');
 
     const count = calculationContainer.childElementCount;
 
-    p.innerHTML = `${count}. ${shapeName} - ${area} cm<sup>2</sup> <button class="btn btn-sm btn-primary">Convert to m<sup>2</sup></button>`;
+    p.innerHTML = `${count}. ${shapeName} - ${area} cm<sup>2</sup> <button class="btn btn-sm btn-primary"> Convert to m<sup>2</sup> </button>`;
 
     calculationContainer.appendChild(p);
 }
 
+function setErrorMessage() {
+    alert('Please type number.');
+}
+
 function area(name, id1, id2) {
     if (name == 'triangle') {
-        const triangleArea = 0.5 * parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
-        if (areaValidation(triangleArea)){
-            addToCalculationEntry(name, triangleArea);
+        const area = 0.5 * parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
+        if (areaValidation(area)) {
+            addToCalculationEntry(name, area);
+            clearInput(id1, id2);
         }
         else {
-            console.log('Please type number.');
+            setErrorMessage();
         }
-
-        clearInput(id1, id2);
     }
     else if (name == 'rectangle') {
-        const rectangleArea = parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
-        addToCalculationEntry(name, rectangleArea);
-
-        clearInput(id1, id2);
+        const area = parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
+        if (areaValidation(area)) {
+            addToCalculationEntry(name, area);
+            clearInput(id1, id2);
+        }
+        else {
+            setErrorMessage();
+        }
     }
     else if (name == 'ellipse') {
-        const ellipseArea = 3.1416 * parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
-        addToCalculationEntry(name, ellipseArea);
-        clearInput(id1, id2);
+        const area = 3.1416 * parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
+        if (areaValidation(area)) {
+            addToCalculationEntry(name, area);
+            clearInput(id1, id2);
+        }
+        else {
+            setErrorMessage();
+        }
     }
     else if (name == 'parallelogram') {
-        const parallelogramArea = parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
-        addToCalculationEntry(name, parallelogramArea);
-        
-
-        clearInput(id1, id2);
+        const area = parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
+        if (areaValidation(area)) {
+            addToCalculationEntry(name, area);
+            clearInput(id1, id2);
+        }
+        else {
+            setErrorMessage();
+        }
     }
     else if (name == 'pentagon') {
-        const pentagonArea = 0.5 * parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
-        addToCalculationEntry(name, pentagonArea);
-
-        clearInput(id1, id2);
+        const area = 0.5 * parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
+        if (areaValidation(area)) {
+            addToCalculationEntry(name, area);
+            clearInput(id1, id2);
+        }
+        else {
+            setErrorMessage();
+        }
     }
     else if (name == 'rhombus') {
-        const rhombus = 0.5 * parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
-        addToCalculationEntry(name, rhombus);
-
-        clearInput(id1, id2);
+        const area = 0.5 * parseFloat(returnValue(id1)) * parseFloat(returnValue(id2));
+        if (areaValidation(area)) {
+            addToCalculationEntry(name, area);
+            clearInput(id1, id2);
+        }
+        else {
+            setErrorMessage();
+        }
     }
 }
