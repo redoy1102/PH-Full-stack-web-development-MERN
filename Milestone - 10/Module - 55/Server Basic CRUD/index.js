bf1685644f0712a909e1575a7f702d9d21a96d70
redoy1102 <hridoyvlog9@gmail.com>
2024-08-11 12:05:10 +0800

    MongoDB and client is added

---
+++ Milestone - 10/Module - 55/Server Basic CRUD/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+
+const uri = "mongodb+srv://codewithredoy:gb4SJy0OuvvykzbF@cluster0.rliqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.log);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Basic CURD is running at ${port}`)
+})
---
+++ Milestone - 9 Authentication/Module - 53.5/Practice Full Stack/my-app-client/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <RouterProvider router={router} />
+  </StrictMode>,
+)
